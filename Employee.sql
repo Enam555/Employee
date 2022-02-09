86a7a1c9472c2f17c44b335869cda76788fa8cb2
Mohammed Enamul Hassan <92625561+Enam555@users.noreply.github.com>
2022-02-09 11:28:31 +0530

    Add files via upload

---
+++ Employee.sql
@@ -0,0 +1,4 @@
+create  database assessment;
+create table Employee ( Id int primary key, FirstName varchar(40), LastName varchar(40), Gender varchar(8), DOB date, Salutation varchar(40), 
+Designation varchar(40), Email varchar(40), Mobile double, AddressLine1 varchar(40),  AddressLine2 varchar(40), City varchar(40), State varchar(40),
+Pin double, Country   varchar(40));
